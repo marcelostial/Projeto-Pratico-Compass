abb4af2f5cdb93b74e656aea734f3ead5093e1e7
hiimarcelo <marceloaface@outlook.com>
2022-02-10 00:40:35 -0300

    feat: finalizando feature climate logical

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import passowrdLogical from "./scripts/password-logical.js";
 import logoutLogical from "./scripts/logout-logical.js";
 import watchLogical from "./scripts/watch-logical.js";
+import climateLogical from "./scripts/climate-logical.js";
 
 passowrdLogical()
 logoutLogical()
-watchLogical()
+climateLogical()
+
+setInterval(() => {
+    watchLogical()
+}, 1000);
